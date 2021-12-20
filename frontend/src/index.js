ddf28f0f2eaca8aa3cc8c1c317e92d1b80247d3c
scode_ken.nguyen_usr <ken@techfox.io>
2021-12-20 10:24:05 +0700

    add debug exercise and description to the user exercise

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,30 +1,43 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-  Link,
-} from 'react-router-dom'
-import Users from './Users'
-import Sum from './Sum'
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Users from "./Users";
+import Sum from "./Sum";
+import HookDebug from "./HookDebug";
 
 ReactDOM.render(
   <BrowserRouter>
     <nav>
-      <ul style={{ display: 'flex', listStyleType: 'none', fontSize: 24, paddingLeft: 0 }}>
-        <li style={{ paddingRight: '1rem' }}>
-          <Link style={{ textDecoration: 'none' }} to='/users'>Users</Link>
+      <ul
+        style={{
+          display: "flex",
+          listStyleType: "none",
+          fontSize: 24,
+          paddingLeft: 0,
+        }}
+      >
+        <li style={{ paddingRight: "1rem" }}>
+          <Link style={{ textDecoration: "none" }} to="/users">
+            Users
+          </Link>
         </li>
-        <li style={{ paddingRight: '1rem' }}>
-          <Link style={{ textDecoration: 'none' }} to='/sum'>Sum</Link>
+        <li style={{ paddingRight: "1rem" }}>
+          <Link style={{ textDecoration: "none" }} to="/sum">
+            Sum
+          </Link>
+        </li>
+        <li style={{ paddingRight: "1rem" }}>
+          <Link style={{ textDecoration: "none" }} to="/hook">
+            Hook
+          </Link>
         </li>
       </ul>
     </nav>
     <Routes>
       <Route path="/users" element={<Users />}></Route>
       <Route path="/sum" element={<Sum />}></Route>
+      <Route path="/hook" element={<HookDebug />}></Route>
     </Routes>
   </BrowserRouter>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
